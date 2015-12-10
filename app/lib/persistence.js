8d9f0319d1d626e05ceea6e442f5b14c5b43604d
Derek Van Hove <d.vanhove@nukama.be>
2015-12-10 15:33:07 +0100

    Reconciliation

--- app/lib/persistence.js
+++ app/lib/persistence.js
@@ -7,6 +7,7 @@ var CheckInsCollection = require('models/collection');
 var collection = new CheckInsCollection();
 var Backbone = require('backbone');
 var connectivity = require('lib/connectivity');
+var _ = require('underscore');
 
 function addCheckIn(checkIn) {
 	checkIn.key = checkIn.key || Date.now();
@@ -17,14 +18,37 @@ function getCheckIns() {
 	return collection.toJSON();
 }
 
-function syncPending() {
-	if (!connectivity.isOnline()) return;
+var pendings = [];
+
+function accountForSync(model) {
+	pendings = _.without(pendings, model);
+	if (pendings.length) return;
+	collection.off('sync', accountForSync);
 	collection.fetch({
 		reset: true
 	});
 }
 
+function syncPending() {
+	if (!connectivity.isOnline()) return;
+	collection.off('sync', accountForSync);
+	pendings = collection.filter(function(c) {
+		return c.isNew();
+	});
+	if (pendings.length) {
+		collection.on('sync', accountForSync);
+		_.invoke(pendings, 'save');
+	} else
+		collection.fetch({
+			reset: true
+		});
+}
+
 syncPending();
+
+Backbone.Mediator.subscribe(
+	'connectivity:online', syncPending
+);
 
 collection.on('reset', function() {
 	Backbone.Mediator.publish('checkins:reset');
